fa34eb1454aa967df94a7c9689f0d589fc21dbd2
Peter deHaan <peter@deseloper.com>
2016-03-11 16:37:25 -0800

    Saddle Up Shawty

--- src/components/Quotes/Quote.js
+++ src/components/Quotes/Quote.js
@@ -3,12 +3,10 @@ import React from 'react';
 import './Quote.scss';
 
 function Quote(props) {
-  const DEFAULT_AUTHOR = `Amy 'ACRad' Chang Radosevich`;
-  const DEFAULT_AVATAR = 'https://scwfit.com/florida/wp-content/uploads/sites/6/2015/12/presenters_amy_chang_radosevich.jpg';
+  const DEFAULT_AUTHOR = 'ACRad';
 
   let {text, author, avatar} = props;
   author = author || DEFAULT_AUTHOR;
-  avatar = avatar || DEFAULT_AVATAR;
 
   return (
     <blockquote>
@@ -16,7 +14,7 @@ function Quote(props) {
         <span className="quote">{text}</span><br/>
         <cite>&mdash; {author}</cite>
       </p>
-      <img src={avatar} alt={author} className="profile-photo" />
+      <img src={avatar} alt="" className="profile-photo" />
     </blockquote>
   );
 }
